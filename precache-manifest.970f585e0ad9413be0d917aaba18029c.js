e69820e9e6dc9918fef2868ad38b425767a29669
pavel <pmykhaleiko@gmail.com>
2020-02-06 12:45:55 +0200

    Updates

---
+++ precache-manifest.970f585e0ad9413be0d917aaba18029c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05a725a656717de29406107935bac0ff",
+    "url": "/search-app/search-app/index.html"
+  },
+  {
+    "revision": "2d42d4eaa8ebb4558f01",
+    "url": "/search-app/search-app/static/css/main.753dbfe8.chunk.css"
+  },
+  {
+    "revision": "0abec2748e74ce9bfda3",
+    "url": "/search-app/search-app/static/js/2.4cd8dee5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/search-app/search-app/static/js/2.4cd8dee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d42d4eaa8ebb4558f01",
+    "url": "/search-app/search-app/static/js/main.2c8c434b.chunk.js"
+  },
+  {
+    "revision": "350e81af37decb487ee8",
+    "url": "/search-app/search-app/static/js/runtime-main.8c047848.js"
+  }
+]);
